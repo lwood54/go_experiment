89df2e65a35e6cc8959fa4cd96e4dfaa42b654c9
Logan Wood <lwood3499@gmail.com>
2020-11-25 10:25:14 -0600

    import and usage of local packages

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,75 @@
 package main
 
 import (
-	"io"
-	"log"
 	"net/http"
 	"strconv"
 
-	"github.com/lwood54/go_experiment/math"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-func main() {
-	numAsString := strconv.Itoa(math.Add(1, 5))
-
-	// Hello world, the web server
-
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, numAsString)
+type (
+	user struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
 	}
+)
 
-	http.HandleFunc("/hello", helloHandler)
-	log.Println("Listing for requests at http://localhost:8000/hello")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+var (
+	users = map[int]*user{}
+	seq   = 1
+)
+
+//----------
+// Handlers
+//----------
+
+func createUser(c echo.Context) error {
+	u := &user{
+		ID: seq,
+	}
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	users[u.ID] = u
+	seq++
+	return c.JSON(http.StatusCreated, u)
+}
+
+func getUser(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	return c.JSON(http.StatusOK, users[id])
+}
+
+func updateUser(c echo.Context) error {
+	u := new(user)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	id, _ := strconv.Atoi(c.Param("id"))
+	users[id].Name = u.Name
+	return c.JSON(http.StatusOK, users[id])
+}
+
+func deleteUser(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	delete(users, id)
+	return c.NoContent(http.StatusNoContent)
+}
+
+func main() {
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.POST("/users", createUser)
+	e.GET("/users/:id", getUser)
+	e.PUT("/users/:id", updateUser)
+	e.DELETE("/users/:id", deleteUser)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/lwood54/go_experiment
 
 go 1.15
 
-require rsc.io/quote v1.5.2
+require (
+	github.com/labstack/echo/v4 v4.1.17
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	golang.org/x/crypto v0.0.0-20201124201722-c8d3bf9c5392 // indirect
+	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b // indirect
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+	golang.org/x/text v0.3.4 // indirect
+	rsc.io/quote v1.5.2
+)
